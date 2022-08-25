6552df0e67923368d8cd074db7de7aed4e98e4d7
WΔ <w@winterdelta.com>
2022-08-25 15:29:26 +0100

    border-width + href

--- components/Info.js
+++ components/Info.js
@@ -5,7 +5,7 @@ export default function Info () {
   return (
     <div className={styles.container}>
       <div className={styles.label}>AI-powered transcription</div>
-      <Link href='/'>
+      <Link href='https://docs.google.com/document/d/1JsKRLWsMte-kEZ1H1YVTBJg4rIaQF0rWn3T9tdu0ZQ4/edit?usp=sharing'>
         <a className={styles.btnLink}>Proposal</a>
       </Link>
     </div>
